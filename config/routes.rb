63ba0374d466d9c9b566bf8fdeb316bafd2bcfbb
nathan-truong-TAMU <nathanthetruong.235@tamu.edu>
2024-09-01 07:42:00 +0000

    Added routing for book deleting confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+    member do
+      get :delete
+    end
+  end
 end
