17f8ac268ef1358ef2d94e7d087ba98fb094a05f
nathan-truong-TAMU <nathanthetruong.235@tamu.edu>
2024-09-09 14:38:13 +0000

    Added different seeding for different environments

---
+++ db/seeds/production.rb
@@ -0,0 +1,32 @@
+Book.create!([
+    {
+        title: "Productioning",
+        author: "The Productioning",
+        price: 99999.99,
+        published_date: Date.new(1999, 12, 31)
+    },
+    {
+        title: "Minecraft: Essential Handbook",
+        author: "Stephanie Milton / Jordan Maron",
+        price: 14.85,
+        published_date: Date.new(2015, 1, 1)
+    },
+    {
+        title: "The Giver",
+        author: "Lois Lowry",
+        price: 11.99,
+        published_date: Date.new(1993, 1, 1)
+    },
+    {
+        title: "The Trumpet of the Swan",
+        author: "E. B. White",
+        price: 9.99,
+        published_date: Date.new(1970, 1, 1)
+    },
+    {
+        title: "The Outsiders",
+        author: "S. E. Hinton",
+        price: 14.99,
+        published_date: Date.new(1967, 4, 24)
+    }
+])
