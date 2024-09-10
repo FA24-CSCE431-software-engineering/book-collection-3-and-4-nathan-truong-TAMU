58fd7fc673fdafd0500e4d7e0bb08737054dbd57
nathan-truong-TAMU <nathanthetruong.235@tamu.edu>
2024-09-10 02:50:01 +0000

    Added User Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users
   resources :books do
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     member do
